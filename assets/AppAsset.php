f7cad9c8e1ffe15549368c3d4be9c8616c5428c1
Nixar <bushmakin59@gmail.com>
2019-06-04 11:42:57 +0500

    Replace yii2-bootstrap with yii2-bootstrap4

--- assets/AppAsset.php
+++ assets/AppAsset.php
@@ -7,7 +7,9 @@
 
 namespace app\assets;
 
+use yii\bootstrap4\BootstrapAsset;
 use yii\web\AssetBundle;
+use yii\web\YiiAsset;
 
 /**
  * Main application asset bundle.
@@ -25,7 +27,7 @@ class AppAsset extends AssetBundle
     public $js = [
     ];
     public $depends = [
-        'yii\web\YiiAsset',
-        'yii\bootstrap\BootstrapAsset',
+        YiiAsset::class,
+        BootstrapAsset::class,
     ];
 }
